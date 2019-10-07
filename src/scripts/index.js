67f75e6f42023da9d7665953a87610d05376f7da
kylianvc <kylianvancanneyt@gmail.com>
2019-10-07 21:45:32 +0200

    oefening klaar oude manier

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,44 @@
 import '../styles/index.scss';
+import L from 'leaflet';
+import "leaflet/dist/leaflet.css";
+import Axios from 'axios';
 
-console.log('webpack starterkit');
+
+
+
+var map = L.map("map", {
+    center: [51.192949, 3.217897],
+    zoom: 15
+});
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?").addTo(map);
+
+var myIcon = L.icon({
+    iconUrl: 'public/marker.png',
+    iconSize: [32, 37],
+    iconAnchor: [16, 37],
+    popupAnchor: [-3, -76]
+});
+
+// L.marker([51.035, 3.217897],{icon: myIcon}).addTo(map);
+
+var hondenWc = [];
+function getData() {
+    Axios.get("https://datatank.stad.gent/4/infrastructuur/hondenvoorzieningen.geojson")
+    .then(function(response){
+        hondenWc = response.data.coordinates;
+        
+        showLocation();
+        console.log(response);
+    });
+}
+
+function showLocation (){
+for (var i = 0; i < hondenWc.length; i++) {
+    const data = hondenWc[i];
+ 
+    L.marker([data[1], data[0]], {icon: myIcon}).addTo(map);
+}
+}
+
+getData();
